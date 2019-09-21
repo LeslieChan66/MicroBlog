5b4152c74f26b328c7c5ac967a48474105d37d10
LeslieChan66 <33823875+LeslieChan66@users.noreply.github.com>
2019-09-21 22:59:05 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,14 +4,14 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 #通用配置
 class Config:
     #密钥
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'jiangxiligong'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or ''
     #数据库的操作
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #邮件发送
-    MAIL_SERVER = os.environ.get("MAIL_SERVER") or 'smtp.126.com'
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") or 'gaohj66666@126.com'
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or 'zxasqw12'
+    MAIL_SERVER = os.environ.get("MAIL_SERVER") or ''
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME") or ''
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or ''
     #bootstrap 使用本地静态文件
     BOOTSTRAP_SERVE_LOCAL = True
     #上传文件 配置
